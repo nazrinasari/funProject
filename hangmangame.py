f96ba3deb98e3c24c9b98b0410251dd03c55b57d
Nazrin Asari <62181607+nazrinasari@users.noreply.github.com>
2024-05-26 15:07:50 +0800

    Update hangmangame.py
    added retry and duplicate input

--- hangmangame.py
+++ hangmangame.py
@@ -7,6 +7,7 @@ def hang_man():
   display = []
   lives = 3
   chose = random.choice(words).lower()
+  guessed = set()
   
   #loop the x number to match the characters count for the hidden words
   for i in range(len(chose)):
@@ -17,6 +18,16 @@ def hang_man():
   while end_of_game == False:
     print(display)
     guess = input("Enter a letter: ").lower()
+    if len(guess) != 1 or not guess.isalpha():
+      print("Enter a single letter only! ")
+      continue
+      
+    if guess in guessed:
+      print("You already enterd the letter", guess)
+      continue
+    guessed.add(guess)
+      
+      
     
     #check if guess is matching with any letter in chose
     #check indexing of the letter is chose, if anything that match with entered letter, if match then replace in display with indexing to match the position
@@ -27,6 +38,7 @@ def hang_man():
           display[index] = guess
     else:
       lives -= 1
+      print(f"Wrong guess, you have {lives} tries left")
   
     if "_" not in display:
       end_of_game = True
@@ -34,7 +46,7 @@ def hang_man():
   
     elif lives == 0:
       end_of_game = True
-      print("You Lose")
+      print("You Lose, the answer is", chose)
 
 
 while True:
